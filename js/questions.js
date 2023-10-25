e3f8a234b81e1c0a798fe5c3e9b76629adb5a9bc
Merrylyn <africangirl800@gmail.com>
2023-10-25 17:49:43 -0500

    fixed my styling and html

---
+++ js/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+      question: "What does CSS stand for?",
+      choices: ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+      correctAnswer: "Cascading Style Sheets"
+    },
+    {
+      question: "What is the symbol for the jQuery library?",
+      choices: ["$", "#", "&", "@"],
+      correctAnswer: "$"
+    },
+    {
+      question: "What is the name of the statement that is used to exit or end a loop?",
+      choices: ["Break statement", "Falter statement", "Conditional statement", "Close statement"],
+      correctAnswer: "Break statement"
+    },
+    {
+      question: "In JavaScript, what element is used to store and manipulate text usually in multiples?",
+      choices: ["Strings", "Arrays", "Function", "Variables"],
+      correctAnswer: "Strings"
+    },
+    {
+      question: "What is the default behavior called that is used to move declarations to the top of the current scope?",
+      choices: ["Jumping", "Sorting", "Hoisting", "Arranging"],
+      correctAnswer: "Hoisting"
+    },
+    {
+      question: "How can you detect the client's browser name?",
+      choices: ["navigator.appName", "client.navName", "browser.name"],
+      correctAnswer: "navigator.appName"
+      },
+    {
+      question: "How do you round the number 7.25, to the nearest integer?",
+      choices: ["Math.round(7.25)", "rnd(7.25)", "round(7.25)", "Math.rnd(7.25)"],
+      correctAnswer: "Math.round(7.25)"
+    },
+    {
+      question: "What will the following code return: Boolean(10>9)?",
+      choices: ["true", "false", "NaN"],
+      correctAnswer: "true"
+    },
+    {
+      question: "Is JavaScript case-sensitive?",
+      choices: ["No", "Yes"],
+      correctAnswer: "Yes"
+    },
+    {
+      question: "Which event occurs when the user clicks on an HTML element?",
+      choices: ["onclick", "onmouseover", "onchange", "onmouseclick"],
+      correctAnswer: "onclick"
+  
+    },
+  ];
